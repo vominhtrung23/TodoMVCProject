776391f273c654e0301d970c4b278edeaf65c9b1
vominhtrung23 <vominhtrung23@gmail.com>
2024-09-24 11:33:33 +0700

    second commit

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -17,9 +17,9 @@ export interface IlistItems {
 
 export class ListItemComponent {
   isComplete = false;
-    isDarkTheme = false;
-    profileForm: FormGroup;
-  constructor(private formBuilder: FormBuilder,private cd: ChangeDetectorRef) {
+  checkToggleAll = false;
+  profileForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) {
     this.profileForm = this.formBuilder.group({
       taskTitle: [''],
       itemsTodos: this.formBuilder.array([]),
@@ -34,86 +34,113 @@ export class ListItemComponent {
     return this.profileForm.get('originalItems') as FormArray;
   }
   onKeydown(event: KeyboardEvent) {
-    // TODO: Use EventEmitter with form value
+
     if (event.key === "Enter" && this.profileForm.value.taskTitle != "") {
       this.addTodos();
       this.profileForm.controls['taskTitle'].setValue("");
     }
+
   }
+
   addTodos() {
-    const lessonForm = this.formBuilder.group({
+    const fieldForm = this.formBuilder.group({
       taskTitle: [this.profileForm.value.taskTitle],
       isComplete: [false]
     });
-    this.itemsTodos.push(lessonForm);
-    this.profileForm.setControl('originalItems', this.formBuilder.array(this.itemsTodos.controls.map(control => control)));
+
+    this.itemsTodos.push(fieldForm);
+    this.addToOriginal();
   }
 
   toggleAll() {
-    this.isDarkTheme = !this.isDarkTheme;
-    this.itemsTodos.controls.forEach( item => {
-        item.patchValue({isComplete : this.isDarkTheme});
-    });
-    
+    this.checkToggleAll = !this.checkToggleAll;
+    this.patchValue(this.itemsTodos);
+    this.patchValue(this.originalItems);
   }
-  onCheckChange(target: any, index: any,item:any) {
-    
-   this.originalItems.value.map((Object:any) => {
+
+  onCheckChange(target: any, index: any, item: any) {
+
+    this.originalItems.value.map((Object: any) => {
       if (Object.taskTitle == item.value.taskTitle) {
-           Object.isComplete = target.checked;
+        Object.isComplete = target.checked;
       }
-  });
-    this.cd.detectChanges();  
+    });
+
+    this.cd.detectChanges();
   }
-  removeItem(index: any,item:any) {
+  removeItem(index: any, item: any) {
     this.itemsTodos.removeAt(index);
     const itemIndex = this.originalItems.value.findIndex((Object: any) => Object.taskTitle == item.value.taskTitle);
-    // Nếu tìm thấy object, xóa object đó
+
     if (itemIndex !== -1) {
-        this.originalItems.removeAt(itemIndex);
+      this.originalItems.removeAt(itemIndex);
     }
-    console.log(" this.originalItems", this.originalItems.value);
-    this.cd.detectChanges(); 
+
+    this.cd.detectChanges();
   }
 
   completed() {
-    const activeItem =  this.originalItems.value.filter((item:any) =>item.isComplete == true);
+    const activeItem = this.originalItems.value.filter((item: any) => item.isComplete == true);
     this.itemsTodos.clear();
-    activeItem.forEach((item:any) => {
-      this.itemsTodos.push(this.formBuilder.group(item)); 
+
+    activeItem.forEach((item: any) => {
+      this.pushObjectToItemsTodos(item);
+
     });
+
     this.cd.detectChanges();
-   
   }
+
   active() {
-    const activeItem =  this.originalItems.value.filter((item:any) =>item.isComplete == false);
+    const activeItem = this.originalItems.value.filter((item: any) => item.isComplete == false);
     this.itemsTodos.clear();
-    activeItem.forEach((item:any) => {
-      this.itemsTodos.push(this.formBuilder.group(item)); 
+
+    activeItem.forEach((item: any) => {
+      this.pushObjectToItemsTodos(item);
     });
+
     this.cd.detectChanges();
   }
+
   all() {
     this.itemsTodos.clear();
-     this.originalItems.controls.forEach(item => {
-      this.itemsTodos.push(this.formBuilder.group(item.value)); 
+
+    this.originalItems.value.forEach((item: any) => {
+      this.pushObjectToItemsTodos(item);
     });
+
     this.cd.detectChanges();
   }
-  clearCompleted(){
-    for (let i = this.itemsTodos.length - 1; i >= 0; i--) {
-      const item = this.itemsTodos.at(i);
-      if (item.value.isComplete) {
-        this.itemsTodos.removeAt(i);
-      }
-    }
-    for (let i = this.originalItems.length - 1; i >= 0; i--) {
-      const item = this.originalItems.at(i);
-      if (item.value.isComplete) {
-        this.originalItems.removeAt(i);
-      }
-    }
-    this.cd.detectChanges(); 
+  
+  clearCompleted() {
+    this.cleaItemInList(this.itemsTodos);
+    this.cleaItemInList(this.originalItems);
+    this.cd.detectChanges();
   }
+
+  pushObjectToItemsTodos(item: any) {
+    this.itemsTodos.push(this.formBuilder.group(item));
+  }
+
+  addToOriginal() {
+    this.profileForm.setControl('originalItems', this.formBuilder.array(this.itemsTodos.controls.map(control => control)));
+  }
+
+  patchValue(listItems: any) {
+    listItems.controls.forEach((item: any) => {
+      item.patchValue({ isComplete: this.checkToggleAll });
+    });
+  }
+
+  cleaItemInList(listItems: any) {
+    for (let i = listItems.length - 1; i >= 0; i--) {
+      const item = listItems.at(i);
+      if (item.value.isComplete) {
+        listItems.removeAt(i);
+      }
+    }
+  }
+
+
 }
 
