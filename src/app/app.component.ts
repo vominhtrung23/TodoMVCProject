60ea0bfed8bd01f2996f280d6b56f45679e8bcc7
vominhtrung23 <vominhtrung23@gmail.com>
2024-09-25 17:23:51 +0700

    split component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,85 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { FormArray, FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ListItemComponent } from "./list-item/list-item.component";
-export interface IlistItems {
-  isComplete: any;
-  taskTitle: any;
-}
+import { FooterComponent } from "./footer/footer.component";
+// export interface IlistItems {
+//   isComplete: any;
+//   taskTitle: any;
+// }
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,ListItemComponent],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule, ListItemComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
+  isComplete = false;
+  checkToggleAll = false;
+  profileForm: FormGroup;
+  itemsTodosValues: any[] = [];
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) {
+    this.profileForm = this.formBuilder.group({
+      taskTitle: [''],
+      itemsTodos: this.formBuilder.array([]),
+      originalItems: this.formBuilder.array([]),
+    });
+  }
+  ngOnInit() {
+  }
+  get itemsTodos(): FormArray {
+    return this.profileForm.get('itemsTodos') as FormArray;
+  }
+  get originalItems(): FormArray {
+    return this.profileForm.get('originalItems') as FormArray;
+  }
+  onKeydown(event: KeyboardEvent) {
+
+    if (event.key === "Enter" && this.profileForm.value.taskTitle != "") {
+      this.addNewTitle();
+      this.profileForm.controls['taskTitle'].setValue("");
+    }
+
+  }
   
+  addNewTitle() {
+    const fieldForm = this.formBuilder.group({
+      taskTitle: [this.profileForm.value.taskTitle],
+      isComplete: [false]
+    });
+
+    this.itemsTodos.push(fieldForm);
+    this.addToOriginal();
+  }
+
+  toggleAll() {
+    this.checkToggleAll = !this.checkToggleAll;
+    this.patchValue(this.itemsTodos);
+    this.patchValue(this.originalItems);
+  }
+  
+  onCheckChange(target: any, index: any, item: any) {
+    this.originalItems.value.map((Object: any) => {
+      if (Object.taskTitle == item.value.taskTitle) {
+        Object.isComplete = target.checked;
+      }
+    });
+    
+    this.cd.detectChanges();
+  }
+  
+  addToOriginal() {
+    this.profileForm.setControl('originalItems', this.formBuilder.array(this.itemsTodos.controls.map(control => control)));
+  }
+
+  patchValue(listItems: any) {
+    listItems.controls.forEach((item: any) => {
+      item.patchValue({ isComplete: this.checkToggleAll });
+    });
+  }
+
 }
--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,75 +1,40 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, Input, Output, EventEmitter, SimpleChanges, forwardRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormArray, FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormControl, ReactiveFormsModule, AbstractControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-export interface IlistItems {
-  isComplete: any;
-  taskTitle: any;
-}
 @Component({
   selector: 'app-list-item',
   standalone: true,
   imports: [RouterOutlet, ReactiveFormsModule, CommonModule, ListItemComponent],
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ListItemComponent),
+      multi: true
+    }
+  ]
 })
 
 export class ListItemComponent {
-  isComplete = false;
-  checkToggleAll = false;
-  profileForm: FormGroup;
-  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) {
-    this.profileForm = this.formBuilder.group({
-      taskTitle: [''],
-      itemsTodos: this.formBuilder.array([]),
-      originalItems: this.formBuilder.array([]),
-    });
-  }
+  @Input() parentForm!: FormGroup;
+  @Input() itemsTodoschild!: any;
+  @Input() originalItems: any;
 
-  get itemsTodos() {
-    return this.profileForm.get('itemsTodos') as FormArray;
-  }
-  get originalItems() {
-    return this.profileForm.get('originalItems') as FormArray;
-  }
-  onKeydown(event: KeyboardEvent) {
-
-    if (event.key === "Enter" && this.profileForm.value.taskTitle != "") {
-      this.addTodos();
-      this.profileForm.controls['taskTitle'].setValue("");
-    }
-
-  }
-
-  addTodos() {
-    const fieldForm = this.formBuilder.group({
-      taskTitle: [this.profileForm.value.taskTitle],
-      isComplete: [false]
-    });
-
-    this.itemsTodos.push(fieldForm);
-    this.addToOriginal();
-  }
-
-  toggleAll() {
-    this.checkToggleAll = !this.checkToggleAll;
-    this.patchValue(this.itemsTodos);
-    this.patchValue(this.originalItems);
-  }
-
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) {}
+  
   onCheckChange(target: any, index: any, item: any) {
-
+    item.patchValue({ isComplete: target.checked });
     this.originalItems.value.map((Object: any) => {
       if (Object.taskTitle == item.value.taskTitle) {
         Object.isComplete = target.checked;
       }
     });
-
     this.cd.detectChanges();
   }
   removeItem(index: any, item: any) {
-    this.itemsTodos.removeAt(index);
+    this.itemsTodoschild.removeAt(index);
     const itemIndex = this.originalItems.value.findIndex((Object: any) => Object.taskTitle == item.value.taskTitle);
 
     if (itemIndex !== -1) {
@@ -79,68 +44,7 @@ export class ListItemComponent {
     this.cd.detectChanges();
   }
 
-  completed() {
-    const activeItem = this.originalItems.value.filter((item: any) => item.isComplete == true);
-    this.itemsTodos.clear();
-
-    activeItem.forEach((item: any) => {
-      this.pushObjectToItemsTodos(item);
-
-    });
-
-    this.cd.detectChanges();
-  }
-
-  active() {
-    const activeItem = this.originalItems.value.filter((item: any) => item.isComplete == false);
-    this.itemsTodos.clear();
-
-    activeItem.forEach((item: any) => {
-      this.pushObjectToItemsTodos(item);
-    });
-
-    this.cd.detectChanges();
-  }
-
-  all() {
-    this.itemsTodos.clear();
-
-    this.originalItems.value.forEach((item: any) => {
-      this.pushObjectToItemsTodos(item);
-    });
-
-    this.cd.detectChanges();
-  }
-  
-  clearCompleted() {
-    this.cleaItemInList(this.itemsTodos);
-    this.cleaItemInList(this.originalItems);
-    this.cd.detectChanges();
-  }
-
-  pushObjectToItemsTodos(item: any) {
-    this.itemsTodos.push(this.formBuilder.group(item));
-  }
-
-  addToOriginal() {
-    this.profileForm.setControl('originalItems', this.formBuilder.array(this.itemsTodos.controls.map(control => control)));
-  }
-
-  patchValue(listItems: any) {
-    listItems.controls.forEach((item: any) => {
-      item.patchValue({ isComplete: this.checkToggleAll });
-    });
-  }
-
-  cleaItemInList(listItems: any) {
-    for (let i = listItems.length - 1; i >= 0; i--) {
-      const item = listItems.at(i);
-      if (item.value.isComplete) {
-        listItems.removeAt(i);
-      }
-    }
-  }
-
-
 }
+
+
 
